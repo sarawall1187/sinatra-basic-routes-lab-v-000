444aaa0956bbca200d9eb144735877ba267efde6
Sara Walker <sarawall1187@gmail.com>
2019-03-07 21:05:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Sara."
+end
+
+get '/hometown' do 
+  "My name is Sara."
+end
+
+get '/favorite-song' do 
+  "My name is Sara."
 end
