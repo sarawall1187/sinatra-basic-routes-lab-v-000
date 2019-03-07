2614613133812aebe0a29793c44744d5dbedf8d5
Sara Walker <sarawall1187@gmail.com>
2019-03-07 21:07:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Sara."
+end
+
+get '/hometown' do
+  "My hometown is Nashville."
+end
+
+get '/favorite-song' do
+  "My favorite song is Bach's Lullaby."
+end
+
 end
